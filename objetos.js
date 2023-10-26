0b010d645be747e339bab66539ec34b8d61566c5
MariaMV81 <mariazuaga31@gmail.com>
2023-10-26 20:30:55 +0200

    Completar ejercicio de objeto

--- objetos.js
+++ objetos.js
@@ -22,9 +22,68 @@ const productos = [
   },
 ];
 
-console.log(productos[0].nombre);
-console.log(productos[1].precio);
+// console.log(productos[0].nombre);
+// console.log(productos[1].precio);
 
-for (let i = 0; i < productos.length; i++){
-    console.log(productos[i].nombre, productos[i].precio)
+for (let i = 0; i < productos.length; i++) {
+  console.log(productos[i].nombre, productos[i].precio);
 }
+
+/**
+ * Calcula la media  de los precios de los productos en el array.
+ */
+
+let precio = 0;
+for (let i = 0; i < productos.length; i++) {
+  precio += productos[i].precio;
+}
+console.log(`La media de los productos es: ${precio / productos.length}`);
+
+/**
+ * Calcula la media  de los precios de los productos en el array.
+ */
+
+let valoraciones = 0;
+for (let i = 0; i < productos.length; i++) {
+  valoraciones += productos[i].valoraciones;
+}
+console.log(
+  `La media de las valoraciones es: ${valoraciones / productos.length}`
+);
+
+/**
+ * Funcion que devuelve el nombre y el stock del producto mas caro
+ */
+
+function buscarNombre(nombre, stock) {
+  let mayor = 0;
+
+  for (let i = 0; i < productos.length; i++) {
+    if (productos[i].precio > mayor) {
+      mayor = productos[i].precio;
+    }
+  }
+
+  for (let i = 0; i < productos.length; i++) {
+    if (productos[i].precio == mayor) {
+      mayor = productos[i].precio;
+      return `El producto más caro es: ${productos[i].nombre} y tiene un stock de: ${productos[i].stock}`
+    }
+
+    
+  }
+  
+
+  return `No existe producto`;
+}
+
+console.log(buscarNombre(productos));
+
+/**
+ * Funcion que recibe un listado de productos y una categoria, debe devolver
+ * array con los productos que son de la categoria del parametro de entrada.
+ *
+ * Ejemplo
+ *
+ * filtrarPorCategoria(productos, "deporte"); debe devolver producto en nombre "bicicleta";
+ */
