ffe6b8bbab7d61fbc1b1c5c60062d9638ddc1a73
MariaMV81 <mariazuaga31@gmail.com>
2023-10-26 20:37:33 +0200

    Incluido enunciado de filtrar por catergoria

---
+++ filtrar-categoria.js
@@ -0,0 +1,8 @@
+/**
+ * Funcion que recibe un listado de productos y una categoria, debe devolver
+ * array con los productos que son de la categoria del parametro de entrada.
+ *
+ * Ejemplo
+ *
+ * filtrarPorCategoria(productos, "deporte"); debe devolver producto en nombre "bicicleta";
+ */
